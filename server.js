e6655d88644d18caf920c6f47b48b538a36accaf
Josh Latour <jlatour88@gmail.com>
2019-08-26 17:22:05 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var mongoose = require('mongoose');
+require('dotenv').config();
+
+// Our scraping tools
+var axios = require('axios');
+var cheerio = require('cheerio');
+
+// Require all models
+var db = require('./models');
+
+// Assign port to server
+var PORT = process.env.PORT || 8080;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static('public'));
+
+// Setup Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Views
+app.set("views", "./views");
+
+// Define routes
+require('./routes/html-routes.js')(app);
+
+// Connect to the Mongo DB
+
+var user = process.env.DB_USER;
+var psw = process.env.DB_PSW;
+var MONGODB_URI =
+   process.env.MONGODB_URI ||
+   `mongodb+srv://${user}:${psw}@webscrapeapp-hoapb.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+
+// Start the server
+app.listen(PORT, function() {
+   console.log(
+      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.  ',
+      PORT,
+      PORT
+   );
+});
+
+// module.exports = app;
