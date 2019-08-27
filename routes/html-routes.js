37d5a14e86dbe55f13589aa062a81ea0ebf1b8ff
Josh Latour <jlatour88@gmail.com>
2019-08-26 19:39:18 -0500

    added duplicate check so we dnt constantly add to db

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,4 @@
+var db = require('../models');
 // Our scraping tools
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -18,7 +19,7 @@ module.exports = function(app) {
                .text()
                .trim();
             result.link = $(el)
-               .find('.entry-title')
+               .find('.entry-title a')
                .attr('href');
             result.date = $(el)
                .find('.date')
@@ -28,19 +29,32 @@ module.exports = function(app) {
                .find('.entry-content')
                .text();
 
-               console.log(result);
+               // console.log(result);
 
             // Insert into DB
-            db.Article.create(result)
-               .then((dbArticle) => {
-                  console.log(dbArticle);
-               })
-               .catch((err) => {
-                  console.log(err);
-               });
+            db.Article.find({
+               link: result.link
+            },
+            function(err, res) {
+               if (err) throw err;
+               // Check if there is a duplicate in DB
+               if (res) {
+                  // If duplicate found, DO NOT add to DB
+                  console.log('duplicate found, do not create again');
+               } else {
+                  // If no duplicate was found, then add to DB
+                  db.Article.create(result)
+                     .then((dbArticle) => {
+                        console.log(dbArticle);
+                     })
+                     .catch((err) => {
+                        console.log(err);
+                     });
+               }
+            });
 
             // Limit results
-            return i<5
+            return i<2
          });
       });
    });
