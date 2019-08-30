5e1d2745db74448b9a30396f08f5086ecde5daf3
Josh Latour <jlatour88@gmail.com>
2019-08-30 11:09:49 -0500

    adjusted comment db  model

--- models/Comment.js
+++ models/Comment.js
@@ -3,10 +3,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CommentSchema = new Schema({
-   // `title` is of type String
-   title: String,
-   // `body` is of type String
-   body: String
+   body: String,
+   commenter: String,
+   date: Date
 });
 
 var Comment = mongoose.model('Comment', CommentSchema);
