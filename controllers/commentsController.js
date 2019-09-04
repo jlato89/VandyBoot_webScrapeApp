061bfec4edb29c16e6c407ad41f9b0b0614c01e5
Josh Latour <jlatour88@gmail.com>
2019-09-04 12:54:40 -0500

    added arrow function

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -9,7 +9,7 @@ var article = require('../models/Article.js');
 //* ADD Comment to article
 router.post('/article/:id', (req, res) => {
    comment.create(req.body)
-      .then(function(dbComment) {
+      .then((dbComment) => {
          // If a Comment was created successfully, find a Article with a _id equal to req.params.id, Then update the Article to be associated with the new Comment
          return article.findOneAndUpdate(
             { _id: req.params.id },
